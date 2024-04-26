bd4f72861b9ed6c2dd658869ab3be2f297f0caed
GenrikhK <gkurmelis@yandex.ru>
2024-04-26 12:05:08 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return 'there will be some info...'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
